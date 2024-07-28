a04fb06cbe82bb239fa6b0afe4df2cfa7998692d
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 17:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	const submitBtn = document.getElementById('submit');
+	submitBtn.addEventListener('click',function(event){
+		event.preventDefault();
+		const firstName = document.getElementById('fname').value;
+		const lastName = document.getElementById('lname').value;
+		const phoneNumber = document.getElementById('phone').value;
+		const emailId = document.getElementById('email').value;
+
+		const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+		alert(alertMessage);
+	})
+})
